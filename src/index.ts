8cd63ef781b712b9be5678e2a6fc46e4d44d4f8b
Oleksandr Oleksyuk <dev.oleksyuk@gmail.com>
2024-06-11 22:09:38 +0200

    chore:  config start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello nodej');
+import express from 'express';
+
+import UserRoutes from './routes/users.routes.js';
+import { port } from './config.js';
+
+const app = express();
+
+// Middleware per il parsing del corpo delle richieste
+app.use(express.json());
+
+// cors headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use('/api', UserRoutes);
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
