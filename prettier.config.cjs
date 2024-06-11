30691dc9248267201bb1557eef097447850d488a
Oleksandr Oleksyuk <dev.oleksyuk@gmail.com>
2024-06-11 17:49:47 +0200

    feat: setup project

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
+
+module.exports = config;
